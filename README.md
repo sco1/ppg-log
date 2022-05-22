252cd2e06a3b338ae5c6b26370be185ea509297b
S. Co1 <sco1.git@gmail.com>
2022-05-22 11:59:51 -0400

    Add initial CLI
    See: #3

--- README.md
+++ README.md
@@ -7,3 +7,60 @@
 [![Open in Visual Studio Code](https://img.shields.io/badge/Open%20in-VSCode.dev-blue)](https://vscode.dev/github.com/sco1/ppg-log)
 
 Helper Tools for PPG FlySight Flight Log Management
+
+🚨 This is an alpha project. User-facing functionality is still under development 🚨
+
+## Installation
+Install from PyPi with your favorite `pip` invocation:
+
+```bash
+$ pip install ppg-log
+```
+
+You can confirm proper installation via the `ppglog` CLI:
+<!-- [[[cog
+import cog
+from subprocess import PIPE, run
+out = run(["ppglog", "--help"], stdout=PIPE, encoding="ascii")
+cog.out(
+    f"```bash\n$ ppglog --help\n{out.stdout.rstrip()}\n```"
+)
+]]] -->
+```bash
+$ ppglog --help
+Usage: ppglog [OPTIONS] COMMAND [ARGS]...
+
+Options:
+  --help  Show this message and exit.
+
+Commands:
+  batch   Batch flight log processing pipeline.
+  single  Single flight log processing pipeline.
+```
+<!-- [[[end]]] -->
+
+## Usage
+🚧 🚧 🚧
+
+## Contributing
+### Development Environment
+This project uses [Poetry](https://python-poetry.org/) to manage dependencies. With your fork cloned to your local machine, you can install the project and its dependencies to create a development environment using:
+
+```bash
+$ poetry install
+```
+
+A [pre-commit](https://pre-commit.com) configuration is also provided to create a pre-commit hook so linting errors aren't committed:
+
+```bash
+$ pre-commit install
+```
+
+### Testing & Coverage
+A [pytest](https://docs.pytest.org/en/latest/) suite is provided, with coverage reporting from [pytest-cov](https://github.com/pytest-dev/pytest-cov). A [tox](https://github.com/tox-dev/tox/) configuration is provided to test across all supported versions of Python. Testing will be skipped for Python versions that cannot be found.
+
+```bash
+$ tox
+```
+
+Details on missing coverage, including in the test suite, is provided in the report to allow the user to generate additional tests for full coverage.
---
+++ ppg_log/cli.py
@@ -0,0 +1,102 @@
+import tkinter as tk
+from pathlib import Path
+from tkinter import filedialog
+
+import click
+import typer
+
+from ppg_log import metrics
+
+ppglog_cli = typer.Typer(add_completion=False)
+
+CURRENT_DIR = Path()
+
+
+def _prompt_for_file(title: str, start_dir: Path = CURRENT_DIR) -> Path:  # pragma: no cover
+    """Open a Tk file selection dialog to prompt the user to select a single file for processing."""
+    root = tk.Tk()
+    root.withdraw()
+
+    picked = filedialog.askopenfilename(  # type: ignore[call-arg]  # stub issue
+        title=title,
+        initialdir=start_dir,
+        multiple=False,
+        filetypes=[
+            ("FlySight Flight Log", "*.csv"),
+            ("All Files", "*.*"),
+        ],
+    )
+
+    if not picked:
+        raise click.ClickException("No file selected for parsing, aborting.")
+
+    return Path(picked)
+
+
+def _prompt_for_dir(start_dir: Path = CURRENT_DIR) -> Path:  # pragma: no cover
+    """Open a Tk file selection dialog to prompt the user to select a directory for processing."""
+    root = tk.Tk()
+    root.withdraw()
+
+    picked = filedialog.askdirectory(
+        title="Select directory for batch processing",
+        initialdir=start_dir,
+    )
+
+    if not picked:
+        raise click.ClickException("No directory selected for parsing, aborting.")
+
+    return Path(picked)
+
+
+@ppglog_cli.command()
+def single(
+    log_filepath: Path = typer.Option(None, exists=True, file_okay=True, dir_okay=False),
+    start_trim: float = typer.Option(metrics.START_TRIM),
+    airborne_threshold: float = typer.Option(metrics.AIRBORNE_THRESHOLD),
+    time_threshold: float = typer.Option(metrics.FLIGHT_LENGTH_THRESHOLD),
+    show_plot: bool = typer.Option(True),
+    plot_save_dir: Path = typer.Option(None, file_okay=True, dir_okay=False),
+) -> None:
+    """Single flight log processing pipeline."""
+    if log_filepath is None:
+        log_filepath = _prompt_for_file(title="Select FlySight Flight Log")
+
+    flight_log = metrics.process_log(
+        log_file=log_filepath,
+        start_trim=start_trim,
+        airborne_threshold=airborne_threshold,
+        time_threshold=time_threshold,
+    )
+    flight_log.summary_plot(show_plot=show_plot, save_dir=plot_save_dir)
+
+
+@ppglog_cli.command()
+def batch(
+    log_dir: Path = typer.Option(None, exists=True, file_okay=False, dir_okay=True),
+    log_pattern: str = typer.Option("*.CSV"),
+    start_trim: float = typer.Option(metrics.START_TRIM),
+    airborne_threshold: float = typer.Option(metrics.AIRBORNE_THRESHOLD),
+    time_threshold: float = typer.Option(metrics.FLIGHT_LENGTH_THRESHOLD),
+    plot_save_dir: Path = typer.Option(None, file_okay=False, dir_okay=True),
+    verbose: bool = typer.Option(True),
+) -> None:
+    """Batch flight log processing pipeline."""
+    if log_dir is None:
+        log_dir = _prompt_for_dir()
+
+    flight_logs = metrics.batch_process(
+        top_dir=log_dir,
+        log_pattern=log_pattern,
+        start_trim=start_trim,
+        airborne_threshold=airborne_threshold,
+        time_threshold=time_threshold,
+        verbose=verbose,
+    )
+
+    for log in flight_logs:
+        log.summary_plot(show_plot=False, save_dir=plot_save_dir)
+
+
+if __name__ == "__main__":  # pragma: no cover
+    ppglog_cli()
